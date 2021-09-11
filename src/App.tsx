43e8ef39a5aac4e24760145aa59dbb6ccf02c80f
josuerojasrojas <josuerojas.rojas@gmail.com>
2021-09-11 14:48:16 -0400

    add more links

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,19 @@ const MyLinks: LinkTree = [
         href: 'https://www.notion.so/withcheesepls/Pelham-Parkway-s-Little-Library-95974c50a3b84027a98aeeeb3ca62f82',
       }
     ]
+  },
+  {
+    header: 'Personal',
+    links: [
+      {
+        title: 'Portfolio',
+        href: 'https://josue-rojas.github.io'
+      },
+      {
+        title: 'Instagram',
+        href: 'http://instagram.com/withcheesepls/'
+      }
+    ]
   }
 ]
 
