47541f309122a7805a09f5a1584397fa26d55814
josue rojas <10749061+josue-rojas@users.noreply.github.com>
2022-04-25 17:09:14 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,10 @@ const MyLinks: LinkTree = [
       {
         title: 'Pelham Parkway Little Library',
         href: 'https://www.notion.so/withcheesepls/Pelham-Parkway-s-Little-Library-95974c50a3b84027a98aeeeb3ca62f82',
+      },
+      {
+        title: 'Survival Kits',
+        href: 'https://azulrojo.com/survivalkits/',
       }
     ]
   },
